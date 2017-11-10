9fff1749d790a4ebe45c2b2bdc2687a31ee68da5
GustavoLastra <gustavo.lastrac@gmail.com>
2017-11-10 16:15:01 +0100

    submit contact to the server

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const values = serializeForm(e.target, { hash: true })
+    if (this.props.onCreateContact)
+      this.props.onCreateContact(values)
+      console.log(values);
+  }
+
   render() {
     return (
       <div>
-        <Link className='close-create-contact' to='/'></Link>
-        <form className='create-contact-form'>
+        <Link className='close-create-contact' to='/'>Close</Link>
+        <form onSubmit={this.handleSubmit} className='create-contact-form'>
           <ImageInput
             className='create-contact-avatar-input'
             name='avatarURL'
