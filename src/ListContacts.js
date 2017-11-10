445971d9f3e83ecadd0c728bb2ee619c3d7d89d3
GustavoLastra <gustavo.lastrac@gmail.com>
2017-11-10 15:30:57 +0100

    till lesson 5 "Form add contact"

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
@@ -15,6 +16,10 @@ class ListContacts extends Component {
 
   updateQuery = (query) => {
     this.setState({ query: query.trim() })
+  }
+
+  clearQuery = () => {
+    this.setState({ query: '' })
   }
 
   render() {
@@ -41,7 +46,19 @@ class ListContacts extends Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
+          <Link
+            to='/create'
+            className='add-contact'
+          >Add Contact</Link>
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+
         <ol className='contact-list'>
           {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
